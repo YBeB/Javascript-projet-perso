84fb46213b83c65936aaa161622245570e72dbd2
ybeb <younes.bouchi@gmail.com>
2024-05-08 19:12:51 +0200

    maj

--- Meteo/script.js
+++ Meteo/script.js
@@ -1,16 +1,37 @@
 
-let url="http://api.weatherapi.com/v1/current.json?key=69ff637606ec4868b0a123953240805&q=Paris"
 // Fonction pour récupérer des données depuis le fichier JSON
 async function fetchData(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Impossible de charger les données");
-      }
-      return await response.json();
-    } catch (error) {
-      console.error("Erreur lors du chargement du JSON", error);
-      return null;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Impossible de charger les données");
     }
-  
+
+    const data= await response.json();
+    console.log(data)
+    return data.location;
+  } catch (error) {
+    console.error("Erreur lors du chargement du JSON", error);
+    return null;
   }
+}
+
+async function displayData(data){
+let countryMeteo=document.querySelector(".localisation");
+let imageMeteo=document.createElement("img")
+
+let meteoData=await fetchData(url);
+if (meteoData){
+countryMeteo.textContent=`${meteoData.country},${meteoData.region},${meteoData.localtime}`
+}
+
+
+
+
+
+
+
+}
+
+displayData()
