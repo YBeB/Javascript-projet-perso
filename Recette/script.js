59c6affebdefb18c8ff5052b6e1be379105ee123
ybeb <younes.bouchi@gmail.com>
2024-05-24 19:57:32 +0200

    maj

---
+++ Recette/script.js
@@ -0,0 +1,57 @@
+let createButton = document.getElementById("create");
+
+function createRecipe() {
+
+let errorTitle=document.querySelector('.title')
+let errorIngredient=document.querySelector('.ingredient')
+let errorStep=document.querySelector('.step')
+
+
+
+
+  let titleValue = document.getElementById("recipe_title").value;
+  let titleInput = document.getElementById("recipe_title");
+  let ingredientValue = document.getElementById("recipe_ingredient").value;
+  let ingredientInput = document.getElementById("recipe_ingredient");
+  let stepValue = document.getElementById("recipe_step").value;
+  let stepInput = document.getElementById("recipe_step");
+  let storedRecipe = localStorage.getItem("Recipe");
+  let parsedStoredRecipe=JSON.parse(storedRecipe)
+if (parsedStoredRecipe==null){
+parsedStoredRecipe=[]
+
+}
+
+  console.log(
+    "titre : " + titleValue,
+    "ingredient : " + ingredientValue,
+    "Etape : " + stepValue
+  );
+  if (titleValue == "") {
+    titleInput.style.border = "red 1px solid";
+    errorTitle.style.display="block"
+  } else {
+    titleInput.style.border = "none";
+    errorTitle.style.display="none"
+  }
+  if (ingredientValue == "") {
+    ingredientInput.style.border = "red 1px solid";
+    errorIngredient.style.display="block"
+  } else {
+    ingredientInput.style.border = "none";
+    errorIngredient.style.display="none"
+  }
+  if (stepValue == "") {
+    stepInput.style.border = "red 1px solid";
+    errorStep.style.display="block";
+  } else {
+    stepInput.style.border = "none";
+    errorStep.style.display="none";
+  }
+}
+
+function deleteRecipe() {}
+
+function displayRecipe() {}
+
+createButton.addEventListener("click", createRecipe);
